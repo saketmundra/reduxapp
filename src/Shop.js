bd27f975f8788f7940b01a6171bf485ed54cbfca
saket <saket@gmail.com>
2022-07-07 14:56:39 +0530

    video 73 cwh

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container'>
+        <h2>Buy Adidas Shoes</h2>
+        <button className='btn btn-primary mx-2'>-</button>
+            Add this item to cart
+        <button className='btn btn-primary mx-2'>+</button>
+
+    </div>
+  )
+}
+
+export default Shop
